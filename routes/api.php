2e88b53e5fbebcc58f3ba8213b7936b0163bcd77
Rido Ananda <ridoananda123@gmail.com>
2021-07-26 19:25:48 +0700

    fix the error and change the dev server

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,16 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\{Route,Artisan};
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
+Route::get('/migrate', function (Request $request) {
+  Artisan::call('migrate');
+  return response()->json(['message'=> 'oke'], 200);
+});
 Route::get('/post', function (Request $request) {
     return $request->user();
 });
+
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
